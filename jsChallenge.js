5bfe552bb963835351187d077a0d021da333ffd5
Martha <marthaduque77@gmail.com>
2024-04-02 16:22:05 -0400

    Test

---
+++ jsChallenge.js
@@ -0,0 +1,39 @@
+/**
+ * Connect this file to your github
+ * you're going to create a "main" branch, then you're going to push this file to your main branch.
+ * NEXT you're going to make a branch called "challenge 1". Solve the first coding challenge, push that branch to github, make a pull request, merge it into main.
+ * NEXT make a branch called "challenge 2" and repeat the above for each coding challenge
+ */
+
+// Challenge 1:
+/**
+ * Console.log all even numbers from 0 - 10
+ * Try to make the solution as efficient as possible. a "for" loop may not be the most efficeint way.
+ */
+
+//Challenge 2:
+// Create a function that reverses an array
+const reverseThis = [1, 'b', 'horse', { name: "Kevin" }, [0, 1, 2]];
+
+//
+//Challenge 3:
+// Sort an array from lowest to highest
+const sortThis = [1, 123, 67342, 2, 43, 64];
+
+
+//Challenge 4:
+// Remove the spaces found in this string
+const stringSpaces = 'hello, my name is, PICKLES!!';
+
+//Challenge 5:
+// Create a function that filters out odd numbers
+/** You'll have a function that takes an array as an input and returns an array */
+
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+const filterNeg = (arr) => {
+
+    return newArr;
+}
+
+// result should be [2, 4, 6, 8, 10];
